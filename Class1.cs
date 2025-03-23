10064290fff56409cb63e7c1ca1ddc9e2944f713
Mohamed Ayoub <mohamed.saad90@aol.com>
2025-03-23 15:23:39 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Forms.Core.Services;
+
+namespace MyCustomLibrary.Controllers
+{
+    public class FormsController : SurfaceController
+    {
+        private readonly IRecordReaderService _recordReaderService;
+
+        public FormsController(IRecordReaderService recordReaderService)
+        {
+            _recordReaderService = recordReaderService;
+        }
+
+
+        public ActionResult GetFormRecords(Guid formId)
+        {
+            if (formId == Guid.Empty)
+            {
+                return Content("Invalid form ID.");
+            }
+
+            var records = _recordReaderService.GetRecordsFromForm(formId, 1, 10);
+
+            if (!records.Items.Any())
+            {
+                return Content("No records found.");
+            }
+
+            var formattedRecords = records.Items.Select(record => new
+            {
+                record.Id,
+                record.Created,
+                Fields = record.RecordFields.ToDictionary(k => k.Key.ToString(), v => v.Value)
+            });
+
+            return Json(formattedRecords, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
